a59d177833bb95694881aa4a6f85a65cf3b7c983
Abhishek Chakravarti <abhishek@taranjali.org>
2022-03-19 15:30:28 +0530

    Define r_yn
    Signed-off-by: Abhishek Chakravarti <abhishek@taranjali.org>

--- src/io.sh
+++ src/io.sh
@@ -16,3 +16,23 @@ r_fail() {
 	printf '[\033[0;31mFAIL\033[0m] %s.\n' "$1"
 	exit
 }
+
+
+# r_yn - prompts user for y/N input
+r_yn() {
+	printf '[\033[0;34m INP\033[0m] %s (y/N)?: ' "$1"
+	read -r _inp
+
+	if [ -z "$_inp" ] ; then
+		r_ok "Operation skipped"
+		return 1
+	fi 
+
+	if [ "$_inp" != "y" ] && [ "$_inp" != "Y" ] ; then
+		r_ok "Operation skipped"
+		return 1
+	fi
+	
+	return 0
+}
+
